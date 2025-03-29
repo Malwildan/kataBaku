965a6afe82f3b1702bce3a502fa830acd17c4ca0
wil <muhammadakmal9187@gmail.com>
2025-03-29 12:27:23 +0700

    Create words.js

---
+++ js/words.js
@@ -0,0 +1,20 @@
+// data.js
+const baku = [
+    "aktivitas",
+    "analisis",
+    "atlet",
+    "praktik",
+    "kualitas"
+    // Add more words...
+];
+
+const tidakBaku = [
+    "aktifitas",
+    "analisa",
+    "atlit",
+    "prakte",
+    "kwalitas"
+    // Add more words...
+];
+
+export { baku, tidakBaku };
